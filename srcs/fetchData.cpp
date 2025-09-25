0758a3dc64eb5877359081ea8fbe0db8edc54812
oel-asri <oel-asri@etu.ac.com>
2025-09-25 18:46:47 +0100

    start implementing makePath function

--- srcs/fetchData.cpp
+++ srcs/fetchData.cpp
@@ -1,12 +1,20 @@
 #include "../Includes/response.hpp"
 #include "../Includes/HttpRequest.hpp"
 
+
+std::string remakePath(HttpRequest request) {
+    
+}
+
+
+
 void getDataFromRequest(HttpRequest request, Response &response){
     Header copyHeader;
 
     response.setMethod(request.getMethod());
-    response.setPath(request.get_path());
-    std::cout << request.get_path() << std::endl; //Rebuild path before assign it to the response object
+    std::string path = remakePath(request); //Rebuild path before assign it to the response object
+    response.setPath(path);
+    std::cout << path << std::endl;
     copyHeader.status_line.HttpVersion = "HTTP/1.1";
     copyHeader.status_line.statusCode = (e_StatusCode) request.getStatusCode();
     copyHeader.status_line.reasonPhrase = getReasonPhrase(copyHeader.status_line.statusCode);
