8f634af486bf5a84a06b61ace846fdcd05c2b806
Riad Elamine <relamine@e3r9p15.1337.ma>
2025-10-07 23:46:08 +0100

    update CGI to handling to type of cgi python and php

---
+++ Includes/Common.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// C++ Standard Library
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <utility>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <cctype>
+#include <cstring>
+#include <ctime>
+#include <stdexcept>
+
+// POSIX / System Headers
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/event.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <dirent.h>
+
+// Project-specific headers
+#include "status_code.hpp"
