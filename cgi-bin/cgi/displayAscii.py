28b2bf63d9779e2399a39798f5db1213b267e2a6
oussama asri <troolmaroc1@gmail.com>
2025-11-08 22:55:21 +0100

    update Makefile, and improve displayAscii.py script

--- cgi-bin/cgi/displayAscii.py
+++ cgi-bin/cgi/displayAscii.py
@@ -1,42 +1,54 @@
-#! /Volumes/KINGSAVE/Webserv/cgi-bin/cgi/env/bin/python3
-
-import sys
-import requests
 from pyfiglet import Figlet
+import os
+import sys
+from urllib.parse import parse_qs
 
 def create_ascii(name, font, color=None):
     fig = Figlet(font=font)
-#    fig = Figlet(font='doom')
     return fig.renderText(name).encode('utf-8')
-#    return fig.renderText(name).encode('ascii', errors='replace')
-#    return f"<pre>\n{ascii_art}</pre>"
+
+# Get content length from environment
+content_length = os.environ.get("CONTENT_LENGTH", "0")
+
+# Parse incoming data
+if content_length and content_length != "0":
+    # Read POST data from stdin
+    post_data = sys.stdin.read(int(content_length))
     
+    # Parse the data (assumes URL-encoded format like: name=oussama&font=doom&color=white)
+    params = parse_qs(post_data)
     
-#def makeBody(name, font, color):
-#    with open("ascii.html") as file:
-#        ascii_text = create_ascii(name, font)
-#        html = file.read()
-#        print(html)
-#
-#makeBody("oussama", 'doom', "white")
-
-ascii_bytes = create_ascii('oussama', 'doom')
-
-# Headers as bytes
-headers = (
-    b"Status: 200 OK\r\n"
-    b"Connection: close\r\n"
-    b"Content-Type: text/plain; charset=utf-8\r\n"
-    b"Content-Length: " + str(len(ascii_bytes)).encode('ascii') + b"\r\n"
-    b"Server: Webserver/1.1.0\r\n"
-    b"\r\n"
-)
-
-# still need to read input file to determine font and name
-
-# Write headers
-sys.stdout.buffer.write(headers)
-
-# Write body
-sys.stdout.buffer.write(ascii_bytes)
-sys.stdout.buffer.flush()
+    # Extract parameters (parse_qs returns lists, so we get first item)
+    name = params.get('text', [''])[0]
+    font = params.get('font', ['doom'])[0]  # default to 'doom'
+    color = params.get('color', [None])[0]
+    
+    # Create ASCII art
+    ascii_bytes = create_ascii(name, font, color)
+    
+    # Headers as bytes
+    headers = (
+        b"Status: 200 OK\r\n"
+        b"Connection: close\r\n"
+        b"Content-Type: text/plain; charset=utf-8\r\n"
+        b"Content-Length: " + str(len(ascii_bytes)).encode('ascii') + b"\r\n"
+        b"Server: Webserver/1.1.0\r\n"
+        b"\r\n"
+    )
+    
+    # Write response
+    sys.stdout.buffer.write(headers)
+    sys.stdout.buffer.write(ascii_bytes)
+else:
+    # No data received
+    error_msg = b"Error: No data received"
+    headers = (
+        b"Status: 400 Bad Request\r\n"
+        b"Connection: close\r\n"
+        b"Content-Type: text/plain; charset=utf-8\r\n"
+        b"Content-Length: " + str(len(error_msg)).encode('ascii') + b"\r\n"
+        b"Server: Webserver/1.1.0\r\n"
+        b"\r\n"
+    )
+    sys.stdout.buffer.write(headers)
+    sys.stdout.buffer.write(error_msg)
--- srcs/Response/fetchData.cpp
+++ srcs/Response/fetchData.cpp
@@ -2,6 +2,7 @@
 #include "../../Includes/utils.hpp"
 #include "../../Includes/Request/HttpRequest.hpp"
 
+
 void getDataFromRequest(HttpRequest &request, Response &response){
     response.fetch_data_from_request((e_StatusCode)request.getStatusCode(), request.getMethod());
 }
